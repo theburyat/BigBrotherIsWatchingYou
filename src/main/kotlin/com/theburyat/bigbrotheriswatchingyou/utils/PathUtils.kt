e4abb9a7cb57500eb38a66d45ff4a8e30a628c4d
theburyat <theburyat16@gmail.com>
2023-01-08 01:25:46 +0300

    change log file location for exam

--- src/main/kotlin/com/theburyat/bigbrotheriswatchingyou/utils/PathUtils.kt
+++ src/main/kotlin/com/theburyat/bigbrotheriswatchingyou/utils/PathUtils.kt
@@ -1,10 +1,22 @@
 package com.theburyat.bigbrotheriswatchingyou.utils
 
+import com.theburyat.bigbrotheriswatchingyou.models.AnalysisContext
 import java.nio.file.Path
+import java.nio.file.Paths
+import java.time.LocalDateTime
 import kotlin.io.path.createTempFile
 
 object PathUtils {
     fun createTempFile(): Path {
-        return createTempFile(suffix = ".log")
+        val homeDirectory = System.getProperty("user.home")
+        val currentDate = LocalDateTime.now()
+
+        val file = Paths.get(homeDirectory, "${currentDate}-${AnalysisContext.studentInfo.name}.log").toFile()
+        if (file.createNewFile()) {
+            return file.toPath()
+        }
+
+        return createTempFile(prefix = homeDirectory, suffix = ".log")
+        //return createTempFile(suffix = ".log")
     }
 }
