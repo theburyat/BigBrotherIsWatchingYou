286f721108963bbdb1eb8e1e1436a80e39e399ab
theburyat <theburyat16@gmail.com>
2023-01-08 15:54:48 +0300

    change log filename so it would not fall on windows

--- src/main/kotlin/com/theburyat/bigbrotheriswatchingyou/utils/PathUtils.kt
+++ src/main/kotlin/com/theburyat/bigbrotheriswatchingyou/utils/PathUtils.kt
@@ -4,12 +4,15 @@ import com.theburyat.bigbrotheriswatchingyou.models.AnalysisContext
 import java.nio.file.Path
 import java.nio.file.Paths
 import java.time.LocalDateTime
+import java.time.format.DateTimeFormatterBuilder
 import kotlin.io.path.createTempFile
 
 object PathUtils {
     fun createTempFile(): Path {
         val homeDirectory = System.getProperty("user.home")
-        val currentDate = LocalDateTime.now()
+        val formatter = DateTimeFormatterBuilder().appendPattern("yyyy-MM-dd_HH-mm-ss").toFormatter()
+
+        val currentDate = LocalDateTime.now().format(formatter)
 
         val file = Paths.get(homeDirectory, "${currentDate}-${AnalysisContext.studentInfo.name}.log").toFile()
         if (file.createNewFile()) {
